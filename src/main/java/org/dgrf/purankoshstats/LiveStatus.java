1f9963afe330e038be7a6cd14e62116c4dad2e51
bhaduri <bhaduri.anriban@gmail.com>
2019-11-11 21:47:58 +0530

    Purankosh Stats

---
+++ src/main/java/org/dgrf/purankoshstats/LiveStatus.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dgrf.purankoshstats;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+
+/**
+ *
+ * @author bhaduri
+ */
+@Named(value = "liveStatus")
+@ViewScoped
+public class LiveStatus  implements Serializable{
+    private LineChartModel lineModel;
+    /**
+     * Creates a new instance of LiveStatus
+     */
+    public LiveStatus() {
+    }
+    @PostConstruct
+    public void init() {
+        createLineModels();
+    }
+
+    public LineChartModel getLineModel() {
+        return lineModel;
+    }
+
+    public void setLineModel(LineChartModel lineModel) {
+        this.lineModel = lineModel;
+    }
+    
+    private LineChartModel initCategoryModel() {
+        LineChartModel model = new LineChartModel();
+ 
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+ 
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
+ 
+        model.addSeries(boys);
+        model.addSeries(girls);
+ 
+        return model;
+    }
+    private void createLineModels() {
+        
+ 
+        lineModel = initCategoryModel();
+        lineModel.setTitle("Category Chart");
+        lineModel.setLegendPosition("e");
+        lineModel.setShowPointLabels(true);
+        lineModel.getAxes().put(AxisType.X, new CategoryAxis("Years"));
+        Axis yAxis = lineModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+ 
+        
+        
+    }
+}
